be3dc31db8dec89382f4289e56da63acd0acff3f
Lucas Debuire <lucas@debuire.fr>
2022-12-03 11:57:11 +0100

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -56,9 +56,63 @@ public class Main {
 		return tab;
 	}
 	
-	static String[] dico= ACX.lectureDico("lib/dico.txt");
+	public static boolean[] corrigerDicoRapide(String[] texte){
+		return corrigerRapide(texte,dico);
+	}
+	
+	public static String[] triDico(String[] t) {
+		for(int i = 1;i<t.length;i++) {
+			if(t[i].compareTo(t[i-1])<0) {
+				int j = i;
+				while((j>0)&&(t[j].compareTo(t[j-1])<0)) {
+					String a = t[j];
+					t[j] = t[j-1];
+					t[j-1] = a;
+					j -= 1;
+				}
+			}
+		} 
+		return t;
+	}
+	
+	static String[] dico= triDico(ACX.lectureDico("lib/dico.txt"));
+	
+	public static int nbrDifference(String s1,String s2) {
+		int dif = 0;
+		if(s1.length() == s2.length()) {
+			for(int i = 0;i<s1.length();i++) {
+				if(s1.charAt(i) != s2.charAt(i)) {
+					dif++;
+				}
+			}
+			return dif;
+		}
+		return -1;
+	}
+	
+	public static String[] proposerCorrection(String m) {
+		int nbrM = 0;
+		String k = "";
+		for(int i = 0;i<dico.length;i++) {
+			if(nbrDifference(dico[i],m) == 1 && dico[i].compareTo(k) != 0) {
+				nbrM++;
+				k = dico[i];
+			}
+		}
+		String[] t = new String[nbrM];
+		int j = 0;
+		k = "";
+		for(int i = 0;i<dico.length;i++) {
+			if(nbrDifference(dico[i],m) == 1 && dico[i].compareTo(k) != 0) {
+				t[j] = dico[i];
+				j++;
+				k = dico[i];
+			}
+		}
+		return t;
+	}
 	
 	public static void main(String[] args){
-		ACX.interfaceCorrection("corrigerDico");
+		ACX.interfaceCorrection("corrigerDicoRapide","proposerCorrection");
 	}
 }
--- testsJUnit/main/CorrecteurTest.java
+++ testsJUnit/main/CorrecteurTest.java
@@ -30,8 +30,8 @@ public class CorrecteurTest{
 	
 	@Test
 	public void test_recherche_4(){
-		String[] t= {"dsf","sdf","dsf"};
-		int resultatRecherche=Main.recherche("sdf",t);
+		String[] t= {"rlf","syf","dsf"};
+		int resultatRecherche=Main.recherche("syf",t);
 		assertEquals(1,resultatRecherche);
 	}
 	
@@ -102,28 +102,28 @@ public class CorrecteurTest{
 	@Test
 	public void test_rechercheRapide_1(){
 		String[] t= {};
-		int resultatRecherche=Main.recherche("cfgh",t);
+		int resultatRecherche=Main.rechercheRapide("cfgh",t);
 		assertEquals(-1,resultatRecherche);
 	}
 	
 	@Test
 	public void test_rechercheRapide_2(){
 		String [] t= {"sfd"};
-		int resultatRecherche=Main.recherche("wxfxd",t);
+		int resultatRecherche=Main.rechercheRapide("wxfxd",t);
 		assertEquals(-1,resultatRecherche);
 	}
 	
 	@Test
 	public void test_rechercheRapide_3(){
 		String [] t= {"xdgf"};
-		int resultatRecherche=Main.recherche("xdgf",t);
+		int resultatRecherche=Main.rechercheRapide("xdgf",t);
 		assertEquals(0,resultatRecherche);
 	}
 	
 	@Test
 	public void test_rechercheRapide_4(){
-		String[] t= {"dsf","sdf","dsf"};
-		int resultatRecherche=Main.recherche("sdf",t);
+		String[] t= {"lsf","sef","dsf"};
+		int resultatRecherche=Main.rechercheRapide("sef",t);
 		assertEquals(1,resultatRecherche);
 	}
 	
@@ -131,7 +131,7 @@ public class CorrecteurTest{
 	public void test_corrigerRapide_1(){
 		String [] t1= {"toiture"};
 		String [] d1= {"toiture"};
-		boolean[] ct= Main.corriger(t1,d1);
+		boolean[] ct= Main.corrigerRapide(t1,d1);
 		assertTrue(ct[0]);
 	}
 	
@@ -139,7 +139,7 @@ public class CorrecteurTest{
 	public void test_corrigerRapide_2(){
 		String [] t1= {"toiture"};
 		String [] d1= {"bateau"};
-		boolean[] ct= Main.corriger(t1,d1);
+		boolean[] ct= Main.corrigerRapide(t1,d1);
 		assertFalse(ct[0]);
 	}
 	
@@ -147,7 +147,7 @@ public class CorrecteurTest{
 	public void test_corrigerRapide_3(){
 		String [] t1= {"toiture","sdfg"};
 		String [] d1= {"toiture"};
-		boolean[] ct= Main.corriger(t1,d1);
+		boolean[] ct= Main.corrigerRapide(t1,d1);
 		assertTrue(ct[0]);
 		assertFalse(ct[1]);
 	}
@@ -156,8 +156,123 @@ public class CorrecteurTest{
 	public void test_corrigerRapide_4(){
 		String [] t1= {"erzr","toiture"};
 		String [] d1= {"toiture"};
-		boolean[] ct= Main.corriger(t1,d1);
+		boolean[] ct= Main.corrigerRapide(t1,d1);
 		assertFalse(ct[0]);
 		assertTrue(ct[1]);
+	}
+	
+	@Test
+	public void test_corrigerDicoRapide_1(){
+		String [] t1= {"je"};
+		boolean[] ct= Main.corrigerDicoRapide(t1);
+		assertTrue(ct[0]);
+	}
+	
+	@Test
+	public void test_corrigerDicoRapide_2(){
+		String [] t1= {"fgff"};
+		boolean[] ct= Main.corrigerDicoRapide(t1);
+		assertFalse(ct[0]);
+	}
+	
+	@Test
+	public void test_corrigerDicoRapide_3(){
+		String [] t1= {"voiture","fgff"};
+		boolean[] ct= Main.corrigerDicoRapide(t1);
+		assertTrue(ct[0]);
+		assertFalse(ct[1]);
+	}
+	
+	@Test
+	public void test_corrigerDicoRapide_4(){
+		String [] t1= {"sefe","voiture"};
+		boolean[] ct= Main.corrigerDicoRapide(t1);
+		assertFalse(ct[0]);
+		assertTrue(ct[1]);
+	}
+	
+	@Test
+	public void test_triDico_1(){
+		String [] t1= {"afg"};
+		String [] t1trie={"afg"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_triDico_2(){
+		String [] t1= {"erf","asf"};
+		String [] t1trie={"asf","erf"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_triDico_3(){
+		String [] t1= {"ert","bnj","aze"};
+		String [] t1trie={"aze","bnj","ert"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_triDico_4(){
+		String [] t1= {"bnj","ert","aze"};
+		String [] t1trie={"aze","bnj","ert"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_triDico_5(){
+		String [] t1= {"aze","bnj","ert"};
+		String [] t1trie={"aze","bnj","ert"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_triDico_6(){
+		String [] t1= {"ert","ert","aze"};
+		String [] t1trie={"aze","ert","ert"};
+
+		Main.triDico(t1);
+		assertArrayEquals(t1trie, t1);
+	}
+	
+	@Test
+	public void test_nbrDifference_1(){
+		int resultat=Main.nbrDifference("vu","v");
+		assertEquals(-1,resultat);
+	}
+	
+	@Test
+	public void test_nbrDifference_2(){
+		int resultat=Main.nbrDifference("nt","nt");
+		assertEquals(0,resultat);
+	}
+	
+	@Test
+	public void test_nbrDifference_3(){
+		int resultat=Main.nbrDifference("bleu","blau");
+		assertEquals(1,resultat);
+	}
+	
+	@Test
+	public void test_nbrDifference_4(){
+		int resultat=Main.nbrDifference("voiture","monruti");
+		assertEquals(5,resultat);
+	}
+	
+	@Test
+	public void test_proposerCorrection_1(){
+		String[] resultat=Main.proposerCorrection("rateau");
+		String[] t = {"bateau","radeau","rameau","râteau"};
+		assertArrayEquals(t,resultat);
 	}
 }	
